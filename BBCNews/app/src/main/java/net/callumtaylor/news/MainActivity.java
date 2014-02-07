db6179797199c0d069523da1fde211348e3b68f0
Callum Taylor <callum@3sidedcube.com>
2014-02-07 08:57:20 +0000

    Linked view with activity

--- BBCNews/app/src/main/java/net/callumtaylor/news/MainActivity.java
+++ BBCNews/app/src/main/java/net/callumtaylor/news/MainActivity.java
@@ -8,5 +8,7 @@ public class MainActivity extends Activity
 	@Override protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.main_view);
 	}
 }
