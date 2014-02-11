6cac605e8cf9036dfd756b2747f077d654990572
Callum Taylor <callum@3sidedcube.com>
2014-02-11 14:03:22 +0000

    Added basic list adapter

--- BBCNews/app/src/main/java/net/callumtaylor/news/MainActivity.java
+++ BBCNews/app/src/main/java/net/callumtaylor/news/MainActivity.java
@@ -2,7 +2,8 @@ package net.callumtaylor.news;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity
 {
@@ -11,5 +12,25 @@ public class MainActivity extends Activity
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.main_view);
+
+		// simple adapter
+		String[] names = {
+			"Andrew Dong",
+			"Callum Taylor",
+			"Dan Gough",
+			"Duncan Cook",
+			"Dave Stockdale",
+			"Martinos",
+			"Matt Cheatham",
+			"Pete Josling",
+			"Phill Caudell",
+			"Steve Adams",
+			"Sam Houghton",
+			"Tom Bell"
+		};
+
+		ListView list = (ListView)findViewById(R.id.list_view);
+		ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, names);
+		list.setAdapter(adapter);
 	}
 }
