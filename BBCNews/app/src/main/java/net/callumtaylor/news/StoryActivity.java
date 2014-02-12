8619396a53e19712c518c54d007700144f20c1eb
Callum Taylor <callum@3sidedcube.com>
2014-02-12 15:23:35 +0000

    Added story activity

---
+++ BBCNews/app/src/main/java/net/callumtaylor/news/StoryActivity.java
@@ -0,0 +1,14 @@
+package net.callumtaylor.news;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StoryActivity extends Activity
+{
+	@Override protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.story_view);
+	}
+}
