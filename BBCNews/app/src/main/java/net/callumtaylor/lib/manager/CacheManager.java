c648f12e7a1e528f1dcbd28654a90fb5e44aad91
Callum Taylor <callum@3sidedcube.com>
2014-03-06 10:10:56 +0000

    Added load/save and exists/modified date methods

--- BBCNews/app/src/main/java/net/callumtaylor/lib/manager/CacheManager.java
+++ BBCNews/app/src/main/java/net/callumtaylor/lib/manager/CacheManager.java
@@ -1,5 +1,14 @@
 package net.callumtaylor.lib.manager;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
 public class CacheManager
 {
 	private static CacheManager INSTANCE;
@@ -12,5 +21,52 @@ public class CacheManager
 		}
 
 		return INSTANCE;
+	}
+
+	public long fileModifiedDate(String filename)
+	{
+		return new File(filename).lastModified();
+	}
+
+	public boolean fileExists(String filename)
+	{
+		return new File(filename).exists();
+	}
+
+	public void save(String filename, Serializable data)
+	{
+		try
+		{
+			BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filename), 8192);
+			ObjectOutputStream os = new ObjectOutputStream(bos);
+			os.writeObject(data);
+			os.flush();
+			os.close();
+			bos.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public Object load(String filename)
+	{
+		try
+		{
+			BufferedInputStream bis = new BufferedInputStream(new FileInputStream(filename), 8192);
+			ObjectInputStream is = new ObjectInputStream(bis);
+			Object data = is.readObject();
+			is.close();
+			bis.close();
+
+			return data;
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		return null;
 	}
 }
